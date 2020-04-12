70c07a36b0fc9be4eba28bd575b877d002a53a97
Fabian Scherer <me@fabianscherer.de>
2020-04-12 18:35:08 +0200

    Bug Fix: spassbeiträge wurden nicht schön angezeigt

--- themes/creativ-preschool-child/archive-spassbeitrag.php
+++ themes/creativ-preschool-child/archive-spassbeitrag.php
@@ -9,56 +9,61 @@
 
 get_header(); ?>
 	<div id="primary" class="content-area">
-        <!-- Where am I: archive-spassbeitrag.php -->
-		<main id="main" class="site-main blog-posts-wrapper" role="main">
+		<!-- Where am I: archive-spassbeitrag.php -->
+		<main id="main" class="site-main" role="main">
 			<div class="">
 
-                <?php
+				<?php
 
-                if ( have_posts() ) {
-	                /* Start the Loop */
+				if ( have_posts() ) {
+					/* Start the Loop */
 
 
-	                $terms = get_terms( array(
-		                'taxonomy'   => 'spasskategorie',
-		                'hide_empty' => false,
-	                ) );
+					$terms = get_terms( array(
+						'taxonomy'   => 'spasskategorie',
+						'hide_empty' => false,
+					) );
 
-	                $irgendwasangeziegt = false;
-	                foreach ( $terms as $sk ) {
-		                global $wp_query;
-		                $args = array_merge( $wp_query->query_vars, array( 'spasskategorie' => $sk->slug ) );
-		                query_posts( $args );
-		                if ( have_posts() ) {
-			                ?>
-                            <div class="section-content clear col-2 archivunterteil archivUnterteiltitel-<?= $sk->slug ?>"><h1 class="archivUnterteiltitel"><a
-                                    href="<?= get_term_link( $sk->slug, "spasskategorie" ) ?>"><?= $sk->name ?></a></h1><?php
+					$irgendwasangeziegt = false;
+					foreach ( $terms as $sk ) {
+						global $wp_query;
+						$args = array_merge( $wp_query->query_vars, array( 'spasskategorie' => $sk->slug ) );
+						query_posts( $args );
+						if ( have_posts() ) {
+							?>
+							<div class="blog-posts-wrapper">
+							<h1 class="archivUnterteiltitel archivUnterteiltitel-<?= $sk->slug ?>"><a
+									href="<?= get_term_link( $sk->slug, "spasskategorie" ) ?>"><?= $sk->name ?></a>
+							</h1>
+							<div
+								class="section-content clear col-3 archivunterteil">
+								<?php
 
-			                while ( have_posts() ) : the_post();
-				                $irgendwasangeziegt = true;
-				                /*
-								 * Include the Post-Format-specific template for the content.
-								 * If you want to override this in a child theme, then include a file
-								 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-								 */
-				                get_template_part( 'template-parts/content', get_post_format() );
+								while ( have_posts() ) : the_post();
+									$irgendwasangeziegt = true;
+									/*
+									 * Include the Post-Format-specific template for the content.
+									 * If you want to override this in a child theme, then include a file
+									 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+									 */
+									get_template_part( 'template-parts/content', get_post_format() );
 
-			                endwhile;
+								endwhile;
 
-			                ?></div><?php
-		                }
-		                wp_reset_query();
-	                }
+								?></div></div><?php
+						}
+						wp_reset_query();
+					}
 
-	                if ( ! $irgendwasangeziegt ) {
-		                get_template_part( 'template-parts/content', 'none' );
-	                }
-                } else {
-	                get_template_part( 'template-parts/content', 'none' );
-                }
-                 ?>
+					if ( ! $irgendwasangeziegt ) {
+						get_template_part( 'template-parts/content', 'none' );
+					}
+				} else {
+					get_template_part( 'template-parts/content', 'none' );
+				}
+				?>
 			</div>
-		<?php the_posts_navigation(); ?>
+			<?php the_posts_navigation(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
--- themes/creativ-preschool-child/date.php
+++ themes/creativ-preschool-child/date.php
@@ -8,36 +8,38 @@
  */
 
 get_header(); ?>
-    <div id="primary" class="content-area">
-        <!-- Where am I: date.php -->
-        <main id="main" class="site-main blog-posts-wrapper" role="main">
-            <div class="section-content clear col-2">
+	<div id="primary" class="content-area">
+		<!-- Where am I: date.php -->
+		<main id="main" class="site-main blog-posts-wrapper" role="main">
+			<div class="section-content clear col-2">
 				<?php
 				if ( have_posts() ) {
 
-					foreach ( array( "fachbeitrag", "gutzuwissenbeitrag", "spassbeitrag", "post") as $posttype ) {
+					foreach ( array( "fachbeitrag", "gutzuwissenbeitrag", "spassbeitrag", "post" ) as $posttype ) {
 						global $wp_query;
 						$args = array_merge( $wp_query->query_vars, array( 'post_type' => $posttype ) );
 						query_posts( $args );
 
 						if ( have_posts() ) {
 							?>
-                            <div class="wrapper archivunterteil"><h1 class="archivUnterteiltitel"><a
-                                    href="<?= get_post_type_archive_link( $posttype ) ?>"><?= get_post_type_object( $posttype )->labels->singular_name ?></a>
-                            </h1><?php
+							<div class="blog-posts-wrapper"><h1 class="archivUnterteiltitel"><a
+									href="<?= get_post_type_archive_link( $posttype ) ?>"><?= get_post_type_object( $posttype )->labels->singular_name ?></a>
+							</h1>
+							<div
+								class="section-content clear col-3 archivunterteil"><?php
 
-							while ( have_posts() ) : the_post();
-								$irgendwasangeziegt = true;
-								/*
-								 * Include the Post-Format-specific template for the content.
-								 * If you want to override this in a child theme, then include a file
-								 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-								 */
-								get_template_part( 'template-parts/content', get_post_format() );
+								while ( have_posts() ) : the_post();
+									$irgendwasangeziegt = true;
+									/*
+									 * Include the Post-Format-specific template for the content.
+									 * If you want to override this in a child theme, then include a file
+									 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+									 */
+									get_template_part( 'template-parts/content', get_post_format() );
 
-							endwhile;
+								endwhile;
 
-							?></div><?php
+								?></div></div><?php
 						}
 						wp_reset_query();
 					}
@@ -49,10 +51,10 @@ get_header(); ?>
 				} else {
 					get_template_part( 'template-parts/content', 'none' );
 				} ?>
-            </div>
+			</div>
 			<?php the_posts_navigation(); ?>
-        </main><!-- #main -->
-    </div><!-- #primary -->
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
 <?php
 get_sidebar();
