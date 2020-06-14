961bb07234c7012eda81854d30d5c1a3736adf46
Fabioni <fabian-scherer98@outlook.de>
2020-06-14 21:05:56 +0200

    Thema Übersicht nutzt jetzt Template von fachbeiträgen

---
+++ themes/creativ-preschool-child/taxonomy-thema.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Creativ Preschool
+ */
+?>
+<!-- Where am I: taxonomy-thema.php -->
+<?php require get_template_directory() . '/archive-fachbeitrag.php'; ?>
